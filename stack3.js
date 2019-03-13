b6230b72cdee403793c2af7d77c6f6410f585934
KimJoon93 <jk1094@naver.com>
2019-03-13 23:57:01 +0900

    Stack study

---
+++ stack3.js
@@ -0,0 +1,14 @@
+function longRunningTask(){
+    // 오래 걸리는 작업
+    console.log('작업 끝');
+}
+
+    console.log('시작');
+     setTimeout(longRunningTask, 0);
+     console.log('다음 작업');
+
+     // 시작
+     // 다음 작업
+     // 작업 끝
+     
+    
