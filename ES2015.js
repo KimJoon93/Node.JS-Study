4fb9120e5b42f1cc54a00e857ab53aaa598c0e70
KimJoon93 <jk1094@naver.com>
2019-03-16 21:08:13 +0900

    Literal String

--- ES2015.js
+++ ES2015.js
@@ -19,3 +19,14 @@ console.log(a);
 let b = 4;
 console.log(b); //let 은 오류 발생안함 대신 블록 밖에서 접근 불가!
 
+
+/* 템플릿 문자열 */
+var num1 = 1;
+var num2 = 2;
+var result = 3;
+
+var string1 = num1 +' 더하기 ' + num2 + '는 \''+ result + '\''; //지저분 ㅡ ㅡ
+console.log(string1); //1 더하기 2는 '3'
+
+const string2 = `${num1} 더하기 ${num2}는 '${result}'` // 백틱 사용, 그리고 세미콜론 필요없다!
+console.log(string2); // 위와 같은 값이 나오는데 더 깔끔
