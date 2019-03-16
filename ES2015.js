74260a035ce6f3f781665f0910dd69f97b0ec5d3
KimJoon93 <jk1094@naver.com>
2019-03-16 20:23:31 +0900

    Const , let Study

--- ES2015.js
+++ ES2015.js
@@ -1,4 +1,4 @@
-/* cons, let **/
+/* const, let **/
 
 if(true){
     var x = 3;
@@ -8,5 +8,14 @@ if(true){
     const y = 3;
 }
 
-console.log(y); 
+// console.log(y); // 블록 안의 y는 접근할 수 없다.
+
+// 보통의 자바스크립트 변수는 var
+// Const 와 let은 블록 밖에서 접근할 수 없음.
+
+const a = 0;
+// a=1; // Const는 한번 대입하면 다른 값을 대입 할 수 없다. 에러발생함!
+console.log(a);
+let b = 4;
+console.log(b); //let 은 오류 발생안함 대신 블록 밖에서 접근 불가!
 
