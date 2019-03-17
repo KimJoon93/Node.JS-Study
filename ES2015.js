0fb01260b7be3ffdb83056d9fe55d0c7a9e2e77d
KimJoon93 <jk1094@naver.com>
2019-03-17 18:12:54 +0900

    Study ES2015

--- ES2015.js
+++ ES2015.js
@@ -1,5 +1,4 @@
 /** Const, let */
-
 if(true){
     var x = 3;
 }
@@ -19,6 +18,7 @@ console.log(a);
 let b = 4;
 console.log(b); //let 은 오류 발생안함 대신 블록 밖에서 접근 불가!
 
+/********************************************************************* */
 
 /** 템플릿 문자열 */
 var num1 = 1;
@@ -30,6 +30,8 @@ console.log(string1); //1 더하기 2는 '3'
 
 const string2 = `${num1} 더하기 ${num2}는 '${result}'` // 백틱 사용, 그리고 세미콜론 필요없다! ', "" 사용가능
 console.log(string2); // 위와 같은 값이 나오는데 더 깔끔, 
+
+/********************************************************************* */
 
 /** 객체 문자열*/
 
@@ -63,6 +65,8 @@ const newObject = {
     newObject.sayNode()
     newObject.sayJS()
     console.log(newObject.ES6)
+
+/********************************************************************* */    
     
 /** 화살표 함수 */ 
 
@@ -92,10 +96,84 @@ var relationship1 = {
     name: 'zero',
     friends: ['nero', 'hero','zero'],
     logFriends: function(){
-        var that = this;
+        var that = this;    // that 이라는 변수를 사용해서 relationship1에 간접적으로 접근
         this.friends.forEach(function(friends){
             console.log(that.name, friends);
         });
     },
 };
 relationship1.logFriends()
+const relationship2 = {
+    name: 'zero',
+    friends: ['nero', 'hero','zero'],
+    logFriends: function(){
+        this.friends.forEach(friend => {    //function을 쓰지 않고 arrow사용
+            console.log(this.name, friend);
+        });
+    },
+};
+relationship2.logFriends()
+
+/********************************************************************* */
+/** Unstructure Allocation*/
+
+var candyMachine = {
+    status : {
+        name: 'node',
+        count: 5,
+    },
+    getCandy: function(){
+        this.status.count--;
+        return this.status.count;
+    }
+};
+
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
+const candyMachine2 = {
+    status: {
+        name: 'node',
+        count: 5,  
+    },
+    getCandy(){
+        this.status.count--;
+        return this.status.count;
+    }
+}
+
+//배열도 비구조화 가능
+var array = ['nodejs', {}, 10, true]
+var node = array[0]
+var obj = array[1]
+var bool = [array.length-1]
+
+//다음과 같이 바꿀 수 있음
+const array1 = ['nodejs', {}, 10, true]
+const [node, obj, , bool] = array
+
+/********************************************************************* */
+
+/** 프로미스 */
+// JS와 노드에서는 주로 비동기 프로그래밍 사용
+// 특히 이벤트 주도 방식때문에 콜백함수 자주 사용. But, 
+// ES2015 부터는 콜백이 아닌 프로미스 기반으로 재구성해서 콜백 헬 극복함
+
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+    if(condition){
+        resolve('성공')
+    }else{
+        reject('실패')
+    }
+});
+
+promise
+    .then((message)=> {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.error(error); 
+    });
+
+    
--- Stack.js
+++ Stack.js
@@ -26,3 +26,43 @@ first()
 // Queue의 경우 순서대로 실행
 // Stack 의 경우 밑에서 부터 쌓이고 가장 위에 있는 것 부터 실행된다. Firtst In Last Out
 
+function run(){
+    console.log('3초 후 실행');
+}
+console.log('시작');
+setTimeout(run, 3000);
+console.log('끝');
+
+
+
+// 실행 결과
+// 시작
+// 끝
+// ...
+// 3초 후 실행
+
+
+// main 이 먼저 쌓이고 그 후에 시작이 호출스택에 쌓이고 실행되고 바로 사라진다 
+// 그 후에 setTimeout이 호출스택에 쌓이게 되는데 이때 콜백인 run은 바로 백그라운드로 올라가고 
+// 3초 뒤에 태스크 큐로 보낸다
+// 그 동안 호출 스택에서 끝 log가 실행되고 난 후 3초 후 실행이 로그에 올라오게 된다. 
+
+function longRunningTask(){
+    // 오래 걸리는 작업
+    for(var i=0; i<50; i++){
+        console.log(i+"번째 작업");
+    }
+    console.log('작업 끝');
+}   
+
+    console.log('시작');
+     setTimeout(longRunningTask, 0);
+     console.log('다음 작업');
+
+     // 시작
+     // 다음 작업
+     // 작업 끝
+     // 
+     //다음작업 콘솔이 먼저 나오는 이유는 setTimeout 실행시 콜백 run은 백그라운드 태스크로 넘어가고
+     //호출스택에 이미 다음작업이 올라간다. 그러므로 작업들은 태스크 큐에서 기다렸다가 올라가게 되므로 
+     //오래걸리는 작업들을 백그라운드에서 돌아가게끔 하여 논블로킹 방식으로 돌아가게 해야한다.
